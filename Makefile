369ba2c6848cd2f1417b4c9b73d1ba0166f496cf
yageek <yannick.heinrich@gmail.com>
2015-11-22 19:11:19 +0100

    Basic LED

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 # AVR Makefile
 
 all:
-	avr-as -mmcu=attiny2313 -o main.out main.S
+	avr-as -mmcu=attiny2313 -o main.out main.asm
 	avr-objcopy -O ihex main.out main.hex
+
+check:
+	avrdude -c stk500v2 -p t2313 -P /dev/tty.usbmodem1421
+
+transfer:
+	 avrdude -c stk500v2 -p t2313 -P /dev/tty.usbmodem1421 -e -U flash:w:main.hex
---
+++ main.asm
@@ -0,0 +1,45 @@
+;
+; ********************************************
+; * avr-sense                                *
+; * [Add more info on software version here] *
+; * (C)2015 by yageek                        *
+; ********************************************
+;
+; Included header file for target AVR type
+
+.INCLUDE "tn2313def.inc"
+
+.section .text
+.ORG 0x00
+
+	rjmp Main
+		
+; ============================================
+;     M A I N    P R O G R A M    I N I T
+; ============================================
+;
+Main:
+
+; ============================================
+;     PORT D Configuration
+; ============================================
+	
+	; All pins of PORTD as Output
+	ldi r16, 0xff
+	ldi r17, 0x00
+	out PORTD, r17
+	out DDRD, r16
+	nop ; Synchronisation
+	cbi PORTD, 0
+	
+
+
+;
+; ============================================
+;         P R O G R A M    L O O P
+; ============================================
+;
+Loop:
+	rjmp Loop ; main loop
+	
+
